1ae78cc4ba404674f8bcced58a0f1e2707c64d60
Michal Kubaščík <michal.kubascik26799@gmail.com>
2023-05-10 09:15:02 +0200

    app.js added content

--- src/App.js
+++ src/App.js
@@ -5,9 +5,72 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <p> Krčmy: </p>
+          <div className="grid-container">
+              <div className="grid-item">
+                  <a href={"http://www.kamnapivo.sk/zilina/vlcince-rosinky/"} target="_blank">Vlky a Rosinky</a>
+                  <p>28 Lipino 28 </p>
+                  <p> Allammo caffe </p>
+                  <p> Beach Club  </p>
+                  <p> Belle Coffee </p>
+                  <p> Berg Vlčince (minuly rok)</p>
+                  <p> Beta  </p>
+                  <p>Bufet NR</p>
+                  <p> Kasava  </p>
+                  <p> Kazačok - Herňa DOXX  </p>
+                  <p> Kúsok šťastia  </p>
+                  <p> Magma  </p>
+                  <p> Majer  </p>
+                  <p> Miláno pizza  </p>
+                  <p> Panelák Cafe  </p>
+                  <p> Pohoda  </p>
+                  <p>  Pool pub  </p>
+                  <p> Sezam  </p>
+                  <p>  Smerr Pub </p>
+                  <p> Star Pub  </p>
+                  <p> Villa Nečas </p>
+                  <p> Vlčí pub  </p>
+                  <p> Vulcano  </p>
+                  <p> Zeppelin pub  </p>
+              </div>
+              <div className="grid-item">
+                  <a href={"http://www.kamnapivo.sk/zilina/hliny-solinky-borik/"} target="_blank">Hliny, Solinky, Borik</a>
+                  <p> Barrique Coffee & Wine Bar  </p>
+                  <p> Bistro u Drobca  </p>
+                  <p> Bôričan  </p>
+                  <p> Bowling Sport Bar Solinky  </p>
+                  <p> City Cafe Bar  </p>
+                  <p> Coffeeshop Company Žilina  </p>
+                  <p> Dreváreň  </p>
+                  <p> Drevená krava  </p>
+                  <p> Družba  </p>
+                  <p> Fun  </p>
+                  <p> Giuseppe  </p>
+                  <p> Jazz caffe Silvia  </p>
+                  <p> Junior  </p>
+                  <p> K2  </p>
+                  <p>  Krčma na ihrisku   </p>
+                  <p>  La Cultura    </p>
+                  <p>   Máj    </p>
+                  <p>  Mystique bar  </p>
+                  <p>  Pellegrino   </p>
+                  <p>  Penzión Kamélia </p>
+                  <p> Rmut - Pivárska bašta </p>
+                  <p> Šariš Pub  </p>
+                  <p>  Smreková koliba </p>
+                  <p>  Stará Menza  </p>
+                  <p>   Štôlňa  </p>
+                  <p>  U Huďana </p>
+                  <p>  Ulala </p>
+                  <p>  Zámoček </p>
+              </div>
+          </div>
           <p>
-              Sem pribudnu linky na krcmy a online koderske prostredie
+              Prostredia:
           </p>
+          <a href={"https://www.programiz.com/python-programming/online-compiler/"} target="_blank">Python</a>
+          <a href={"https://www.programiz.com/java-programming/online-compiler/"} target="_blank">Java</a>
+          <a href={"https://www.programiz.com/c-programming/online-compiler/"} target="_blank">C</a>
       </header>
     </div>
   );
